7251a8197c7853327140e42a41ac5314dc3a8114
RISHI SUNKARI <rishi250904@gmail.com>
2025-12-25 09:18:25 +0530

    Update handlesdata.js

--- handlesdata.js
+++ handlesdata.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/youtubeharsh', {
+mongoose.connect('mongodb://localhost:27017/contesttracking, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
